cbc39741df7443bc89509bedcbe6895e375aaa72
Russell <russhwolf@gmail.com>
2019-08-26 23:29:11 -0400

    Infix funs in top-level plugins block

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,8 @@
  */
 
 plugins {
-    kotlin("multiplatform").version("1.3.50").apply(false)
-    id("com.android.library").version("3.4.1").apply(false)
+    kotlin("multiplatform") version "1.3.50" apply false
+    id("com.android.library") version "3.4.1" apply false
 }
 
 subprojects {
