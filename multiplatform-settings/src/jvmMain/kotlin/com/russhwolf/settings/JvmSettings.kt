dc9694da2dfde53d83e79c278ced285a06c3f0b9
Russell <russhwolf@gmail.com>
2019-10-01 22:10:22 -0400

    Various JVM adjustments

--- multiplatform-settings/src/jvmMain/kotlin/com/russhwolf/settings/JvmSettings.kt
+++ multiplatform-settings/src/jvmMain/kotlin/com/russhwolf/settings/JvmSettings.kt
@@ -16,18 +16,19 @@
 
 package com.russhwolf.settings
 
+import com.russhwolf.settings.JvmPreferencesSettings.Factory
 import java.util.Properties
 import java.util.prefs.PreferenceChangeEvent
 import java.util.prefs.PreferenceChangeListener
 import java.util.prefs.Preferences
 
 @Deprecated(
-    message = "Use JvmPropertiesSettings or JvmPreferencesSettings to disambiguate usage.",
-    replaceWith = ReplaceWith("JvmPropertiesSettings", "com.russhwolf.settings.JvmPropertiesSettings")
+    message = "Use JvmPreferencesSettings or JvmPropertiesSettings to disambiguate usage.",
+    replaceWith = ReplaceWith("JvmPreferencesSettings", "com.russhwolf.settings.JvmPreferencesSettings")
 )
 @ExperimentalJvm
 @Suppress("UNUSED", "KDocMissingDocumentation")
-typealias JvmSettings = JvmPropertiesSettings
+typealias JvmSettings = JvmPreferencesSettings
 
 /**
  * A collection of storage-backed key-value data
@@ -151,7 +152,8 @@ public class JvmPropertiesSettings public constructor(
  * Operator extensions are defined in order to simplify usage. In addition, property delegates are provided for cleaner
  * syntax and better type-safety when interacting with values stored in a `Settings` instance.
  *
- * On the JVM platform, this class can be created by passing a [Preferences] instance which will be used as a delegate.
+ * On the JVM platform, this class can be created by passing a [Preferences] instance which will be used as a delegate,
+ * or via a [Factory]
  *
  * This class is experimental as marked by the [ExperimentalJvm] annotation.
  */
@@ -181,49 +183,49 @@ public class JvmPreferencesSettings public constructor(
 
     public override fun remove(key: String): Unit = delegate.remove(key)
 
-    public override fun hasKey(key: String): Boolean = delegate.keys().contains(key)
+    public override fun hasKey(key: String): Boolean = key in delegate.keys()
 
     public override fun putInt(key: String, value: Int): Unit = delegate.putInt(key, value)
 
     public override fun getInt(key: String, defaultValue: Int): Int = delegate.getInt(key, defaultValue)
 
     public override fun getIntOrNull(key: String): Int? =
-        if (delegate.keys().contains(key)) delegate.getInt(key, 0) else null
+        if (key in delegate.keys()) delegate.getInt(key, 0) else null
 
     public override fun putLong(key: String, value: Long): Unit = delegate.putLong(key, value)
 
     public override fun getLong(key: String, defaultValue: Long): Long = delegate.getLong(key, defaultValue)
 
     public override fun getLongOrNull(key: String): Long? =
-        if (delegate.keys().contains(key)) delegate.getLong(key, 0L) else null
+        if (key in delegate.keys()) delegate.getLong(key, 0L) else null
 
     public override fun putString(key: String, value: String): Unit = delegate.put(key, value)
 
     public override fun getString(key: String, defaultValue: String): String = delegate.get(key, defaultValue)
 
     public override fun getStringOrNull(key: String): String? =
-        if (delegate.keys().contains(key)) delegate.get(key, "") else null
+        if (key in delegate.keys()) delegate.get(key, "") else null
 
     public override fun putFloat(key: String, value: Float): Unit = delegate.putFloat(key, value)
 
     public override fun getFloat(key: String, defaultValue: Float): Float = delegate.getFloat(key, defaultValue)
 
     public override fun getFloatOrNull(key: String): Float? =
-        if (delegate.keys().contains(key)) delegate.getFloat(key, 0f) else null
+        if (key in delegate.keys()) delegate.getFloat(key, 0f) else null
 
     public override fun putDouble(key: String, value: Double): Unit = delegate.putDouble(key, value)
 
     public override fun getDouble(key: String, defaultValue: Double): Double = delegate.getDouble(key, defaultValue)
 
     public override fun getDoubleOrNull(key: String): Double? =
-        if (delegate.keys().contains(key)) delegate.getDouble(key, 0.0) else null
+        if (key in delegate.keys()) delegate.getDouble(key, 0.0) else null
 
     public override fun putBoolean(key: String, value: Boolean): Unit = delegate.putBoolean(key, value)
 
     public override fun getBoolean(key: String, defaultValue: Boolean): Boolean = delegate.getBoolean(key, defaultValue)
 
     public override fun getBooleanOrNull(key: String): Boolean? =
-        if (delegate.keys().contains(key)) delegate.getBoolean(key, false) else null
+        if (key in delegate.keys()) delegate.getBoolean(key, false) else null
 
     @ExperimentalListener
     public override fun addListener(key: String, callback: () -> Unit): SettingsListener {
