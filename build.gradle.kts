e327b211a08d0dc1dea8f3d8fd0cc0abba4087fb
Russell <russhwolf@gmail.com>
2020-04-20 18:04:22 -0400

    Workaround for name collision issue with K/N libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,15 @@ allprojects {
         mavenCentral()
         jcenter()
     }
+
+    // Workaround for potential name collisions with default module name (https://youtrack.jetbrains.com/issue/KT-36721)
+    pluginManager.withPlugin("kotlin-multiplatform") {
+        val kotlinExtension = project.extensions.getByName("kotlin") as KotlinMultiplatformExtension
+        val uniqueName = "${project.group}.${project.name}"
+        kotlinExtension.targets.withType(KotlinNativeTarget::class.java) {
+            compilations["main"].kotlinOptions.freeCompilerArgs += listOf("-module-name", uniqueName)
+        }
+    }
 }
 
 apiValidation {
