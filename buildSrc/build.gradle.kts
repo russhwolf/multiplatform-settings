9172b1c71977aa7b9508ac46e5def89998d03960
Russell <russhwolf@gmail.com>
2020-11-01 23:32:11 -0500

    Update to Kotlin 1.4.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,7 +15,7 @@
  */
 
 plugins {
-    kotlin("jvm") version "1.4.0"
+    kotlin("jvm") version "1.4.10"
 }
 
 repositories {
--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -15,8 +15,8 @@
  */
 
 plugins {
-    kotlin("multiplatform") version "1.4.0" apply false
-    kotlin("android") version "1.4.0" apply false
+    kotlin("multiplatform") version "1.4.10" apply false
+    kotlin("android") version "1.4.10" apply false
     id("com.android.library") version "4.0.1" apply false
     id("com.android.application") version "4.0.1" apply false
 }
