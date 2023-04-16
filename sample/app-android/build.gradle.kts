bfd3e8e59c6deba9fd74b1ac69aa7dd4d6ea9039
Fred Porciúncula <tfcporciuncula@gmail.com>
2023-04-16 14:13:42 +0200

    Reimplement UI in Compose

--- sample/app-android/build.gradle.kts
+++ sample/app-android/build.gradle.kts
@@ -69,7 +69,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.activity:activity-compose:1.7.0")
     implementation("androidx.compose.material:material-icons-core")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("com.russhwolf:multiplatform-settings:${rootProject.ext["library_version"]}")
 }
