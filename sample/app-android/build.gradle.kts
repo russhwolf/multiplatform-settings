a871dc23044a0c894a54c614b1b44ce44ba333d5
Fred Porciúncula <tfcporciuncula@gmail.com>
2023-04-16 12:48:24 +0200

    Add Compose dependencies
    Just following what’s here https://developer.android.com/jetpack/compose/setup#kotlin

--- sample/app-android/build.gradle.kts
+++ sample/app-android/build.gradle.kts
@@ -30,7 +30,7 @@ android {
 
     defaultConfig {
         applicationId = "com.russhwolf.settings.example.android"
-        minSdk = 15
+        minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -45,11 +45,27 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
 }
 
 dependencies {
     implementation(project(":shared"))
     implementation(fileTree("include" to listOf("*.jar"), "dir" to "libs"))
+    implementation(platform("androidx.compose:compose-bom:2023.09.02"))
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("com.russhwolf:multiplatform-settings:${rootProject.ext["library_version"]}")
